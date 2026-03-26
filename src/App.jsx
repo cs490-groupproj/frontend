636142bfae0662ec59cd428b854eca917acbb4ab
SerkanUt <utogluserkan@gmail.com>
2026-03-26 19:48:05 -0400

    fixed app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,42 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import Navbar from "./components/Navbar.jsx";
 import SignIn from "./components/SignIn.jsx";
 import SignUp from "./components/SignUp.jsx";
 import ClientSurvey from "./components/ClientSurvey.jsx";
-function AppRoutes() {
-  const location = useLocation();
-  const authPaths = ["/"];
-  const showNavbar = !authPaths.includes(location.pathname);
+import Sidebar from "./components/Sidebar.jsx";
+import ClientDashboard from "./components/ClientDashboard.jsx";
+import BrowseCoaches from "./components/BrowseCoaches.jsx";
 
+
+const App = () => {
   return (
-    <>
-      {showNavbar && <Navbar />}
+   <BrowserRouter>
+      <div className="flex min-h-screen bg-background text-foreground">
+        
+      
+        <Sidebar />
+        
+  
+        <main className="flex-1 p-8 overflow-y-auto">
       <Routes>
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/clientsurvey" element={<ClientSurvey />} />
         <Route path="/pathname1" element={<div>Navigation component</div>} />
         <Route path="/pathname2" element={<div>Navigation component</div>} />
+        <Route path="/" element={<ClientDashboard />} />
+        <Route path="/coaches" element={<BrowseCoaches />} />
+        <Route path="/exercises" element={<div className="p-4">Exercises Content</div>} />
+        <Route path="/payment" element={<div className="p-4">Payment Content</div>} />
+        <Route path="/profile" element={<div className="p-4">Edit Profile Content</div>} />
+        <Route path="/chat" element={<div className="p-4">Chat Content</div>} /> 
       </Routes>
-    </>
-  );
-}
-
-const App = () => {
-  return (
-    <BrowserRouter>
-      <AppRoutes />
+        </main>
+      </div>
     </BrowserRouter>
   );
 };
+
+
 
 export default App;
