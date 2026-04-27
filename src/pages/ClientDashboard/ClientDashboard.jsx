b8bc2c2fa67a34b442216464db8dc8ac4a86882f
SerkanUt <utogluserkan@gmail.com>
2026-04-26 21:00:20 -0400

    Added before and after pics

--- src/pages/ClientDashboard/ClientDashboard.jsx
+++ src/pages/ClientDashboard/ClientDashboard.jsx
@@ -3,6 +3,7 @@ import useGetFromAPI from "@/hooks/useGetFromAPI";
 import WeeklySchedule from "./components/WeeklySchedule";
 import Stats from "./components/Stats";
 import DailySurvey from "./components/DailySurvey";
+import ProgressPics from "./components/progressPics";
 
 export default function ClientDashboard() {
   const [activeTab, setActiveTab] = useState("weeklySchedule");
@@ -62,11 +63,22 @@ export default function ClientDashboard() {
         >
           Daily Survey
         </button>
+        <button
+          onClick={() => setActiveTab("progressPics")}
+          className={`px-4 py-2 text-sm font-medium focus:outline-none ${
+            activeTab === "progressPics"
+              ? "border-border-600 text-text border-b-2"
+              : "text-gray-500 hover:text-gray-700"
+            }`}
+        >
+          Progress Pics
+        </button>
       </div>
 
       {activeTab === "dailySurvey" && <DailySurvey />}
       {activeTab === "stats" && <Stats />}
       {activeTab === "weeklySchedule" && <WeeklySchedule />}
+      {activeTab === "progressPics" && <ProgressPics />}
     </div>
   );
 }
