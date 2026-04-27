c2ccdd627f40f229de35e7bf95d21089f95321c4
Unoboomo <jddjuch@gmail.com>
2026-04-27 15:20:57 -0400

    rebuilt topcoaches with API integration

--- src/pages/LandingPage/components/TopCoaches.jsx
+++ src/pages/LandingPage/components/TopCoaches.jsx
@@ -1,30 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CoachCard from "@/features/coach/CoachCard";
-
-const coach_infos = [
-  {
-    name: "Sam Sulek",
-    rating: 4.9,
-    reviewCount: 100,
-    specializations: "Fitness and Nutrition",
-    certifications: ["Cert1", "Cert2", "Cert3", "Cert4"],
-    showFooter: false,
-    cost: 70,
-    pfp: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQn2qdy1EPnEmlMWJQ1YF4FADireaUT0Bg4eA&s",
-  },
-  {
-    name: "Ronnie Coleman",
-    rating: 4.8,
-    reviewCount: 84,
-    specializations: "Fitness",
-    certifications: ["Cert1", "Cert2", "Cert3", "Cert4"],
-    showFooter: false,
-    cost: 75,
-    pfp: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT38kZ9LNLwlrldWWCumGKOS9cOHRxnwGiZTg&s",
-  },
-];
+import useGetFromAPI from "@/hooks/useGetFromAPI";
 
 const TopCoaches = () => {
+  const [coachData, setCoachData] = useState([]);
+  const [coachesURI, setCoachesURI] = useState("/visitors/top_coaches?limit=6");
+  const SPECIALIZATIONS = {
+    ALL: "ALL",
+    EXERCISE: "EXERCISE",
+    NUTRITION: "NUTRITION",
+    BOTH: "BOTH",
+  };
+
+  const mapCoachFromBackend = (coach) => {
+    const rating = Number(coach.avg_rating ?? 0);
+    return {
+      name: `${coach.first_name} ${coach.last_name}`,
+
+      specializations: [
+        coach.is_exercise_specialization && SPECIALIZATIONS.EXERCISE,
+        coach.is_nutrition_specialization && SPECIALIZATIONS.NUTRITION,
+      ].filter(Boolean),
+
+      qualifications: coach.qualifications,
+
+      rating: rating,
+      isUnrated: rating === 0,
+    };
+  };
+  const {
+    data: coachesData,
+    loading: coachesLoading,
+    error: coachesError,
+  } = useGetFromAPI(coachesURI, null);
+
+  useEffect(() => {
+    if (!coachesData || !coachesData.coaches) {
+      return;
+    }
+    setCoachData(coachesData.coaches.map(mapCoachFromBackend));
+  }, [coachesData]);
+
   return (
     <section className="flex flex-col items-center gap-8 py-16">
       <div className="flex flex-col items-center gap-4 pb-4">
@@ -33,12 +49,17 @@ const TopCoaches = () => {
           Blah Blah Blah filler text
         </p>
       </div>
-
-      <div className="flex w-full shrink-0 flex-wrap justify-center gap-4">
-        {coach_infos.map((coach_info) => (
-          <CoachCard key={coach_info.name} coach_info={coach_info} />
-        ))}
-      </div>
+      {coachesError ? (
+        <p>error: {coachesError}</p>
+      ) : coachesLoading ? (
+        <p>Loading Coaches</p>
+      ) : (
+        <div className="grid gap-5 sm:grid-cols-2 xl:grid-cols-3">
+          {coachData.map((coach_info) => (
+            <CoachCard key={coach_info.name} coach_info={coach_info} />
+          ))}
+        </div>
+      )}
     </section>
   );
 };
